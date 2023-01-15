7e3f48f210b250c9946a7e1a3517d670146ce55b
JR <jadedResearcher@gmail.com>
2023-01-15 16:16:46 -0500

    basic template ready

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 
+const fruit_source = `http://farragofiction.com/DollSource/images/Fruit/Body/`;
 
+let fruit = [];
 window.onload = () => {
 
   initThemes();
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  let themes = urlParams.get('themes');
-  let name = urlParams.get('name');
-  let fee = parseInt(urlParams.get('feeUnder'));
+  startFruitSelling();
+  
+}
 
-  if (name !== null || themes !== null) {
-    handleRestaurantPage(name, themes, stringtoseed(name),fee);
-  } else {
-    handleCategories();
-  }
-  initCart();
-
-
-
+startFruitSelling = async ()=>{
+  fruit = await getImages(fruit_source);
 }
 
 
